d41b3cd7a737da1cc5e44442d418d5eb729eadbe
Ola Krassowska <ola.krassowska@hotmail.com>
2024-09-21 17:46:31 +0200

    Added auth

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 
 <script>
-
-import HelloWorld from './components/HelloWorld.vue'
-import M from 'materialize-css'
-
 export default {
   name: 'App',
-  mounted () {
-    M.AutoInit()
-  },
-  components: {
-    HelloWorld
-  }
-}
+};
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'; 
 
 import 'materialize-css/dist/css/materialize.min.css'
 import 'material-design-icons/iconfont/material-icons.css'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount('#app')
